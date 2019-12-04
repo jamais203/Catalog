675c71e636fabe5e87e3364611f6849651050537
jamais203 <58527936+jamais203@users.noreply.github.com>
2019-12-04 20:46:36 +0100

    Add data in the memory DB

---
+++ data.sql
@@ -0,0 +1,9 @@
+INSERT INTO product (id,title, price, description) VALUES
+(1,'Serviette de bain', 20, 'Pour un séchage rapide et doux'),
+(2,'Roue de secours', 50, 'Parce qu''on a toujours besoin d''une roue de secours'),
+(3,'Croquette pour chien 3.5kg', 25, 'Offrez le meilleur à votre chien, les croquettes qualidad+'),
+(4,'Calendrier de l''avent', 10, 'Un chocolat pour chaque jour de décembre'),
+(5,'Le livre de la jungle en blu ray', 26, 'Le dessin animé original de 1994'),
+(6,'Doudoune', 225, 'Doudoune de combat pour les températures extrêmes'),
+(7,'Tasse à café', 7, 'Pour se réchauffer les mains et le coeur'),
+(8,'Sac à dos', 48, 'Un sac à dos waterproof');
