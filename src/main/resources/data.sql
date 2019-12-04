aa7729e46ca56846b7e3b456d97d453c4fcf401b
jamais203 <58527936+jamais203@users.noreply.github.com>
2019-12-04 21:38:47 +0100

    Update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,7 +3,7 @@ INSERT INTO product (id,title, price, description) VALUES
 (2,'Roue de secours', 50, 'Parce qu''on a toujours besoin d''une roue de secours'),
 (3,'Croquette pour chien 3.5kg', 25, 'Offrez le meilleur à votre chien, les croquettes qualidad+'),
 (4,'Calendrier de l''avent', 10, 'Un chocolat pour chaque jour de décembre'),
-(5,'Le livre de la jungle en blu ray', 26, 'Le dessin animé original de 1994'),
-(6,'Doudoune', 225, 'Doudoune de combat pour les températures extrêmes'),
-(7,'Tasse à café', 7, 'Pour se réchauffer les mains et le coeur'),
-(8,'Sac à dos', 48, 'Un sac à dos waterproof');
+(5,'Le livre de la jungle en blu ray', 26, 'Le classique de Disney de 1994'),
+(6,'Doudoune', 225, 'Doudoune de combat pour affronter le froid'),
+(7,'Carnet de note', 7, 'Carnet de note de poche pour prendre des notes'),
+(8,'Sac de voyage', 78, 'Un sac de voyage waterproof');
